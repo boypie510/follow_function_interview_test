8b105e2b6f6efa4d3688d178a18a5fb8322e0c1e
boypie510 <boypie510@gmail.com>
2023-06-11 16:51:36 +0800

    green light for student_request_rspec follow action

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,6 +3,17 @@
 # handle /students endpoint
 class StudentsController < ApplicationController
   def follow
+    teacher.followed_students << student
     render json: { message: 'Teacher successfully followed the student' }, status: :ok
+  end
+
+  private
+
+  def teacher
+    @teacher ||= Teacher.find(params[:teacher_id])
+  end
+
+  def student
+    @student ||= Student.find(params[:id])
   end
 end
