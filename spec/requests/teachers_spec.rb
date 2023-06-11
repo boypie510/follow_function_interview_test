71f70056c65c17cc383f7a0353c53dfa28d6f7b7
boypie510 <boypie510@gmail.com>
2023-06-11 18:18:40 +0800

    add expected_result for GET/followed_students

--- spec/requests/teachers_spec.rb
+++ spec/requests/teachers_spec.rb
@@ -9,11 +9,28 @@ RSpec.describe 'Teachers', type: :request do
 
   describe 'GET /followed_students' do
     context 'when teacher already follow the students' do
+      before { teacher.followed_students << [student1, student2] }
       context 'when the teacher exist' do
+        let(:expected_result) do
+          {
+            students: [
+              {
+                id: student1.id,
+                name: student1.name
+              },
+              {
+                id: student2.id,
+                name: student2.name
+              }
+            ]
+          }.as_json
+        end
         it 'list teacher followed students' do
           get "/teachers/#{teacher.id}/followed_students"
+          json_body = JSON.parse(response.body)
 
           expect(response).to have_http_status(:ok)
+          expect(json_body).to eq(expected_result)
         end
       end
     end
