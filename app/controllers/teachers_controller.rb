f6b356f9207a8e3b460d34d2fe8b9cecdde48ac9
boypie510 <boypie510@gmail.com>
2023-06-11 18:19:03 +0800

    pass the followed_students rspec

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -2,7 +2,15 @@
 
 # handle /teachers endpoint
 class TeachersController < ApplicationController
+  before_action :find_teacher, only: :followed_students
+
   def followed_students
-    render json: {}, status: :ok
+    render json: { students: @teacher.followed_students.select(:id, :name) }, status: :ok
+  end
+
+  private
+
+  def find_teacher
+    @teacher = Teacher.find(params[:id])
   end
 end
