7eacd33e890a3d77371218e53ff0cd3012c28d69
boypie510 <boypie510@gmail.com>
2023-06-11 17:39:31 +0800

    add unfollow rspec case when teacher/student not exist

--- spec/requests/students_spec.rb
+++ spec/requests/students_spec.rb
@@ -40,11 +40,33 @@ RSpec.describe 'Students', type: :request do
   describe 'DELETE /unfollow' do
     context 'teacher already follow the student' do
       before { teacher.followed_students << student }
-      it 'teacher unfollow the student' do
-        delete "/students/#{student.id}/unfollow", params: { teacher_id: teacher.id }
+      context 'when the student and teacher exist' do
+        it 'teacher unfollow the student' do
+          delete "/students/#{student.id}/unfollow", params: { teacher_id: teacher.id }
 
-        expect(response).to have_http_status(:ok)
-        expect(teacher.followed_students.ids).not_to include(student.id)
+          expect(response).to have_http_status(:ok)
+          expect(teacher.followed_students.ids).not_to include(student.id)
+        end
+      end
+
+      context 'when the teacher does not exist' do
+        it 'returns a not found error' do
+          not_exist_teacher_id = 9999
+          delete "/students/#{student.id}/unfollow", params: { teacher_id: not_exist_teacher_id }
+
+          expect(response).to have_http_status(:not_found)
+          expect(response.body).to include('Teacher not found')
+        end
+      end
+
+      context 'when the student does not exist' do
+        it 'returns a not found error' do
+          not_exist_student_id = 9999
+          delete "/students/#{not_exist_student_id}/unfollow", params: { teacher_id: teacher.id }
+
+          expect(response).to have_http_status(:not_found)
+          expect(response.body).to include('Student not found')
+        end
       end
     end
   end
