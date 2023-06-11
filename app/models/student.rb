f6b2fc782345ee704d26ac669eddce8d8876bd18
boypie510 <boypie510@gmail.com>
2023-06-11 23:25:35 +0800

    add validate name for student/teacher model

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,4 +3,6 @@
 class Student < ApplicationRecord
   has_many :teacher_following
   has_many :following_teachers, through: :teacher_following, source: :teacher
+
+  validates :name, presence: true
 end
