ded649dc4f145fea8baaad22f8fe1e441e03bd84
boypie510 <boypie510@gmail.com>
2023-06-11 17:04:32 +0800

    pass rspec case for student not found

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,20 +2,25 @@
 
 # handle /students endpoint
 class StudentsController < ApplicationController
+  before_action :find_teacher, only: :follow
+  before_action :find_student, only: :follow
+
   def follow
-    teacher.followed_students << student
+    @teacher.followed_students << @student
     render json: { message: 'Teacher successfully followed the student' }, status: :ok
-  rescue ActiveRecord::RecordNotFound
-    render json: { error: 'Teacher not found' }, status: :not_found
   end
 
   private
 
-  def teacher
+  def find_teacher
     @teacher ||= Teacher.find(params[:teacher_id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'Teacher not found' }, status: :not_found
   end
 
-  def student
+  def find_student
     @student ||= Student.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'Student not found' }, status: :not_found
   end
 end
